99b32d36b8eb41164f57f02a052e3c2ca20a6080
Aaron Lippold <lippold@gmail.com>
2026-02-12 22:14:37 -0500

    feat: add password policy resource, refactor 3 IAM controls
    OciIdentityDomainPasswordPolicies fetches password policies from
    identity domains. Provides FilterTable columns for min_length,
    min_numerals, min_special_chars, password_expires_after, and
    num_passwords_in_history.
    
    Controls refactored: 1.4, 1.5, 1.6
    
    Authored by: Aaron Lippold<lippold@gmail.com>

---
+++ libraries/oci_identity_domain_password_policies.rb
@@ -0,0 +1,68 @@
+require_relative 'oci_backend'
+
+class OciIdentityDomainPasswordPolicies < OciCollectionResourceBase
+  name 'oci_identity_domain_password_policies'
+  desc 'Fetches password policies from OCI Identity Domains.'
+
+  filter_table_config = FilterTable.create
+  filter_table_config.register_column(:domain_urls,              field: :domain_url)
+  filter_table_config.register_column(:policy_ids,               field: :policy_id)
+  filter_table_config.register_column(:min_lengths,              field: :min_length)
+  filter_table_config.register_column(:min_numerals_list,        field: :min_numerals)
+  filter_table_config.register_column(:min_special_chars_list,   field: :min_special_chars)
+  filter_table_config.register_column(:password_expires_afters,  field: :password_expires_after)
+  filter_table_config.register_column(:num_passwords_in_histories, field: :num_passwords_in_history)
+  filter_table_config.install_filter_methods_on_resource(self, :table)
+
+  def initialize(opts = {})
+    @tenancy_ocid = opts[:tenancy_ocid]
+    super
+  end
+
+  def fetch_data
+    return [] if @tenancy_ocid.to_s.empty?
+
+    rows = []
+    domain_urls = fetch_domain_urls
+
+    domain_urls.each do |domain_url|
+      policies = oci_cli_raw(
+        %(oci identity-domains password-policies list --endpoint "#{domain_url}" --all)
+      ).dig('data', 'resources') || []
+
+      policies.each do |policy|
+        next unless %w[StandardPasswordPolicy PasswordPolicy].include?(policy['id'])
+
+        rows << {
+          domain_url: domain_url,
+          policy_id: policy['id'],
+          min_length: safe_int(policy['min-length']),
+          min_numerals: safe_int(policy['min-numerals']),
+          min_special_chars: safe_int(policy['min-special-chars']),
+          password_expires_after: safe_int(policy['password-expires-after']),
+          num_passwords_in_history: safe_int(policy['num-passwords-in-history'])
+        }
+      end
+    end
+
+    rows
+  end
+
+  def to_s
+    'OCI Identity Domain Password Policies'
+  end
+
+  private
+
+  def fetch_domain_urls
+    cmd = %(oci iam domain list --compartment-id '#{@tenancy_ocid}' --all | jq '[.data[] | .url]')
+    result = inspec.json(command: "#{cmd} 2>/dev/null").params
+    result.is_a?(Array) ? result : []
+  rescue StandardError
+    []
+  end
+
+  def safe_int(value)
+    value&.to_i
+  end
+end
